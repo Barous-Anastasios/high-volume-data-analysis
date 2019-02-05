446ff4fa8d204f6862f8d4870b74ce082dd96330
unknown <tasosbarous@gmail.com>
2019-02-05 21:34:26 +0200

    new

--- src/ml.py
+++ src/ml.py
@@ -13,8 +13,24 @@ from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall
 from sklearn.model_selection import KFold
 from sklearn.metrics import classification_report, confusion_matrix,  auc
 
+from scipy.linalg import svd
+
+from gensim.test.utils import common_texts
+from gensim.sklearn_api import D2VTransformer
+
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
+
+
 # ------------------------ OUTPUT ------------------------ #
-output = pd.DataFrame(
+EvaluationMetric_10fold = pd.DataFrame(
     columns = [
         "SVM (BoW)",
         "RANDOM FOREST (BoW)",
@@ -34,28 +50,12 @@ output = pd.DataFrame(
     ]
 )
 
-output.to_csv("./../dist/EvaluationMetric_10fold.csv", sep='\t')
-
-# ------------------------ MY CLASSIFICATION ------------------------ #
-test_df = pd.read_csv("./../data/test_set.csv", sep = "\t")
-df = pd.read_csv("./../data/train_set.csv", sep = "\t")
-df['Content'] = df.Content.map(lambda x: x.lower().translate(str.maketrans('','', string.punctuation)))
-
-X_train, X_test, y_train, y_test = train_test_split(df['Content'], df['Category'], random_state = 1)
-print("Training dataset: ", X_train.shape[0])
-print("Test dataset: ", X_test.shape[0])
-
-X_scaled = preprocessing.scale(X_train)
-
-print('\n\nBAG OF WORDS \n')
-count_vector = CountVectorizer(stop_words = 'english')
-training_data_bow = count_vector.fit_transform(X_scaled)
-testing_data_bow = count_vector.transform(X_test)
-randomForestClassifier = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=0)
-randomForestClassifier.fit(training_data_bow, y_train)
-y_pred = randomForestClassifier.predict(testing_data_bow)
-print(cross_val_score(randomForestClassifier, training_data_bow, y_train, cv=10))
-print(cross_val_predict(randomForestClassifier, training_data_bow, y_train, cv=10))
+testSet_categories = pd.DataFrame(
+    columns=[
+        "Test_Document_ID",
+        "Predicted_Category"
+    ]
+)
 
 # ------------------------ PREPARING THE 10-FOLD VALIDATION ------------------------ #
 kfold = KFold(n_splits=10, random_state=42)
@@ -64,89 +64,87 @@ scoring = {'accuracy' : make_scorer(accuracy_score),
            'recall' : make_scorer(recall_score),
            'f1_score' : make_scorer(f1_score)}
 
-# ------------------------ RANDOM FORESTS ------------------------ #
 test_df = pd.read_csv("./../data/test_set.csv", sep = "\t")
 df = pd.read_csv("./../data/train_set.csv", sep = "\t")
 df['Content'] = df.Content.map(lambda x: x.lower().translate(str.maketrans('','', string.punctuation)))
-
 X_train, X_test, y_train, y_test = train_test_split(df['Content'], df['Category'], random_state = 1)
-print("Training dataset: ", X_train.shape[0])
-print("Test dataset: ", X_test.shape[0])
 
-print('\n\nBAG OF WORDS \n')
+# ------------------------ FEATURES ------------------------ #
 count_vector = CountVectorizer(stop_words = 'english')
+svd = TruncatedSVD(n_components = 90, algorithm = 'arpack')
+doc2vec = D2VTransformer(min_count=1, size=5)
+
+# ------------------------ CLASSIFIERS ------------------------ #
+randomForestClassifier = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=0)
+svclassifier = SVC(kernel='linear')
+
 training_data_bow = count_vector.fit_transform(X_train)
+testing_data_bow = count_vector.fit_transform(X_test)
+# randomForestClassifier.fit(training_data_bow, y_train)
+# y_pred = randomForestClassifier.predict(testing_data_bow)
+
+training_data_svd = svd.fit_transform(X_train)
+testing_data_svd = svd.fit_transform(X_test)
+# randomForestClassifier.fit(training_data_svd, y_train)
+# y_pred = randomForestClassifier.predict(testing_data_svd)
+
+training_data_d2v = doc2vec.fit_transform(X_train)
+testing_data_d2v = doc2vec.fit_transform(X_test)
+# randomForestClassifier.fit(training_data_d2v, y_train)
+# y_pred = svclassifier.predict(testing_data_d2v)
+
+data_mine = preprocessing.scale(X_train)
+training_data_bow = count_vector.fit_transform(data_mine)
 testing_data_bow = count_vector.transform(X_test)
 randomForestClassifier = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=0)
-randomForestClassifier.fit(training_data_bow, y_train)
-y_pred = randomForestClassifier.predict(testing_data_bow)
+# randomForestClassifier.fit(training_data_bow, y_train)
+# y_pred = randomForestClassifier.predict(testing_data_bow)
+
+print(cross_val_score(randomForestClassifier, training_data_bow, y_train, cv=10))
+print(cross_val_predict(randomForestClassifier, training_data_bow, y_train, cv=10))
+print(cross_val_score(RandomForestClassifier, training_data_svd, y_train, cv=2))
+print(cross_val_predict(RandomForestClassifier, training_data_svd, y_train, cv=10))
+print(cross_val_score(RandomForestClassifier, training_data_d2v, y_train, cv=10))
+print(cross_val_predict(RandomForestClassifier, testing_data_d2v, y_train, cv=10))
+
+print(cross_val_score(svclassifier, training_data_bow, y_train, cv=10))
+print(cross_val_predict(svclassifier, training_data_bow, y_train, cv=10))
+print(cross_val_score(svclassifier, training_data_svd, y_train, cv=2))
+print(cross_val_predict(svclassifier, training_data_svd, y_train, cv=10))
+print(cross_val_score(svclassifier, training_data_d2v, y_train, cv=10))
+print(cross_val_predict(svclassifier, testing_data_d2v, y_train, cv=10))
+
 print(cross_val_score(randomForestClassifier, training_data_bow, y_train, cv=10))
 print(cross_val_predict(randomForestClassifier, training_data_bow, y_train, cv=10))
 
-print('\n\nSINGULAR VALUE DECOMPOSITION \n')
-svd = TruncatedSVD(n_components = 90, algorithm = 'arpack')
-training_data_svd = svd.fit_transform(X_train)
-testing_data_svd = svd.transform(X_test)
-randomForestClassifier = RandomForestClassifier(kernel='linear')
-randomForestClassifier.fit(training_data_svd, y_train)
-y_pred = randomForestClassifier.predict(testing_data_svd)
-print(cross_val_score(RandomForestClassifier, training_data_svd, y_train, cv=2))
-print(cross_val_predict(RandomForestClassifier, training_data_svd, y_train, cv=10))
+# ------------------------ OUTPUTTING ------------------------ #
+EvaluationMetric_10fold.to_csv("./../dist/EvaluationMetric_10fold.csv", sep='\t')
+testSet_categories.to_csv("./../dist/testSet_categories.csv", sep='\t')
 
-print('\n\nDOC TO VEC \n')
-training_data_dtv = doc2vec.Doc2Vec(X_train, size = 100, window = 300, min_count = 1, workers = 4)
-testing_data_dtv = doc2vec.Doc2Vec(X_test, size = 100, window = 300, min_count = 1, workers = 4)
-svclassifier = RandomForestClassifier(kernel='linear')
-svclassifier.fit(training_data_dtv, y_train)
-y_pred = svclassifier.predict(testing_data_dtv)
-print(cross_val_score(RandomForestClassifier, training_data_dtv, y_train, cv=10))
-print(cross_val_predict(RandomForestClassifier, training_data_dtv, y_train, cv=10))
+# PLOT
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(5):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
 
-# ------------------------ SUPPORT VECTOR MACHINES ------------------------ #
-test_df = pd.read_csv("./../data/test_set.csv", sep = "\t")
-df = pd.read_csv("./../data/train_set.csv", sep = "\t")
-df['Content'] = df.Content.map(lambda x: x.lower().translate(str.maketrans('','', string.punctuation)))
-
-X_train, X_test, y_train, y_test = train_test_split(df['Content'], df['Category'], random_state = 1)
-print("Training dataset: ", X_train.shape[0])
-print("Test dataset: ", X_test.shape[0])
-
-print('\n\nBAG OF WORDS \n')
-count_vector = CountVectorizer(stop_words = 'english')
-training_data_bow = count_vector.fit_transform(X_train)
-testing_data_bow = count_vector.transform(X_test)
-svclassifier = SVC(kernel='linear')
-svclassifier.fit(training_data_bow, y_train)
-y_pred = svclassifier.predict(testing_data_bow)
-print(cross_val_score(svclassifier, training_data_bow, y_train, cv=10))
-print(cross_val_predict(svclassifier, training_data_bow, y_train, cv=10))
-
-print('\n\nSINGULAR VALUE DECOMPOSITION \n')
-svd = TruncatedSVD(n_components = 90, algorithm = 'arpack')
-training_data_svd = svd.fit_transform(X_train)
-testing_data_svd = svd.transform(X_test)
-svclassifier = SVC(kernel='linear')
-svclassifier.fit(training_data_svd, y_train)
-y_pred = svclassifier.predict(testing_data_svd)
-print(cross_val_score(svclassifier, training_data_svd, y_train, cv=10))
-print(cross_val_predict(svclassifier, training_data_svd, y_train, cv=10))
-
-print('\n\nDOC TO VEC \n')
-training_data_dtv = doc2vec.Doc2Vec(X_train, size = 100, window = 300, min_count = 1, workers = 4)
-testing_data_dtv = doc2vec.Doc2Vec(X_test, size = 100, window = 300, min_count = 1, workers = 4)
-svclassifier = SVC(kernel='linear')
-svclassifier.fit(training_data_dtv, y_train)
-y_pred = svclassifier.predict(testing_data_dtv)
-print(cross_val_score(svclassifier, training_data_dtv, y_train, cv=10))
-print(cross_val_predict(svclassifier, training_data_dtv, y_train, cv=10))
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
 
-
-# OTHER ---------------------------------------------------------------
-# print(confusion_matrix(y_test,y_pred))
-# print(classification_report(y_test,y_pred))
-# print("Accuracy score: ", accuracy_score(y_test, y_pred))
-# print("Recall score: ", recall_score(y_test, y_pred, average = 'weighted'))
-# print("Precision score: ", precision_score(y_test, y_pred, average = 'weighted'))
-# print("F1 score: ", f1_score(y_test, y_pred, average = 'weighted'))
-# print("AUC: ", auc(y_test, y_pred))
+# Plot of a ROC curve for a specific class
+plt.figure()
+lw = 2
+plt.plot(fpr[2], tpr[2], color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
